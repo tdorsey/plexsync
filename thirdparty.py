925dccbe30435779d7d3efd2d2b90c1460c0fc2f
Tommy Dorsey <tdorsey@gmail.com>
2017-09-06 21:00:17 -0500

    retrieve media info from third party

--- thirdparty.py
+++ thirdparty.py
@@ -1,7 +1,6 @@
 import enum
 import requests
 from base import *
-
 settings = getSettings()
 
 class ThirdPartyService(enum.Enum):
@@ -12,15 +11,14 @@ class ThirdParty():
     def __init__(self, service):
         self.service = service
         try:
-          self.apiKey = settings.get(service, 'api-key')
           self.host = settings.get(service, 'host')
+          self.api_root = "api" 
+          self.endpoint_base = f"{self.host}/{self.api_root}/"
+          self.endpoints = {"lookup": None, "profile" : "profile", "rootfolder" : "rootfolder" }
+          self.apiKey = settings.get(service, 'api-key')
           self.headers = {'X-Api-Key': self.apiKey}
           self.quality_profiles = self._getQualityProfiles()
           self.root_folder = self._getRootfolder()  
-          self.api_root = "api" 
-          self.endpoints = dict()
-          self.endpoint_base = f"{self.host}/{api_root}/"
-          self.endpoints = dict({"lookup": None, "profile" : "profile", "rootfolder" : "rootfolder" })    
           if self.service == ThirdPartyService.Show.value:
                 self.endpoints["lookup"] = "series/lookup"
           elif self.service == ThirdPartyService.Movie.value:
@@ -31,22 +29,22 @@ class ThirdParty():
         except:
           print(f"Add your {service} api key to the config file")
 
-    def _buildURL(endpoint):
+    def _buildURL(self, endpoint):
         return str(f"{self.endpoint_base}{endpoint}") 
 
     def lookupMedia(self, media):
        print("thirdparty is ")
        dump(self)   
        param = {'term' : media.search_term}
-       response = requests.get(url = self._buildURL(self.endpoints["lookup"], params = param, headers = self.headers))
+       response = requests.get(url = self._buildURL(self.endpoints["lookup"]), params = param, headers = self.headers)
        print(f"Searching for: {media.title} - with {media.search_term}")
        return response.json()
     
     def _getQualityProfiles(self):
-         response = requests.get(url = self._buildURL(self.endpoints["profile"], headers = self.headers))    
+         response = requests.get(url = self._buildURL(self.endpoints["profile"]), headers = self.headers)
          return response.json()
     
     def _getRootfolder(self):
-         response = requests.get(url = self._buildURL(self.endpoints["rootfolder"], headers = self.headers))    
+         response = requests.get(url = self._buildURL(self.endpoints["rootfolder"]), headers = self.headers)    
          return response.json()
         
