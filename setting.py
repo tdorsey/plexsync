f6d6d14dd48d10cc116b3dc91af1f76610905c09
Tommy Dorsey <tdorsey@gmail.com>
2017-09-18 14:01:49 -0500

    Support adding only selected items instead of the entire list. closes #5

--- setting.py
+++ setting.py
@@ -1,5 +1,18 @@
 class Setting():
-    def __init__(self, key, prompt):
+    def __init__(self, key, section, prompt):
         self.key = key
         self.value = None 
         self.prompt = prompt
+        self.section = section
+
+    def write(self):
+            print(f"setting {self.key} in {self.section}")
+
+            user_input = input(self.prompt)
+            self.value = str(user_input)
+
+            settings.set(_section, setting.key, setting.value)
+            writeSettings(settings)
+            return setting.value
+
+
