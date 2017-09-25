c0cabb0aec6adfbc96117cf6be39f5e7b76926bd
Tommy Dorsey <tdorsey@users.noreply.github.com>
2017-09-25 10:40:59 -0500

    refactor plexsync to be available for web as well as cli. Create cli version

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request, redirect, url_for, abort, session
+from plexsync import *
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'changeme'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/servers', methods=['POST'])
+def servers():
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+
+    plexAccount = plexsync.getAccount(session['username'], session['password'])
+    servers = plexsync.getServers(plexAccount)    
+
+    return render_template('servers.html',server_list=servers)
+    #return redirect(url_for('message'))
+
+@app.route('/message')
+def message():
+    if not 'username' in session:
+        return abort(403)
+    return render_template('templates/message.html', username=session['username'], 
+                                           message=session['message'])
+
+if __name__ == '__main__':
+    app.run()
