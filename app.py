d1c04d2d67888f4d740fad64769c62f50fc2b566
Tommy Dorsey <tdorsey@gmail.com>
2017-09-29 23:12:04 -0500

    Adjust import paths

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, request, redirect, url_for, abort, session, jsonify, g
-from plexsync.plexsync import PlexSync
+from plexsync import *
 import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'changeme'
