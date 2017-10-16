c7d1ff60571fb18e9abe5b98dc49ede8fbbe3491
Tommy Dorsey <tdorsey@gmail.com>
2017-10-16 15:48:27 -0500

    Side by side list

--- static/main.js
+++ static/main.js
@@ -1,9 +1,10 @@
     function onSelectSection(e) {
-        var server = $('.server').val();
-        var section = $('.section').val();
+        var that = this;
+        var section = $(this).val();
+        var server = $(this).siblings(".server").val();
         var endpoint = $SCRIPTROOT + '/servers/' + server + '/' + section
         $.post(endpoint, { server : server, section : section }, function(response) {
-            sectionMedia = $('.media')
+            sectionMedia = $(that).siblings(".media")
             sectionMedia.empty()
             $.each(response, function(index, item) {
                 sectionMedia.append('<li>' + item + '</li>');
@@ -12,18 +13,20 @@
 
 } 
     function onSelectServer(e) {
-        
+        var that = this;
         var server = $(this).val();
-        var endpoint = $SCRIPTROOT + '/servers/' + server
+        var endpoint = $SCRIPTROOT + '/servers/' + server;
         $.post(endpoint, { server : server }, function(response) {
-            sectionSelect = $('.section')
-            sectionSelect.empty()
+            sectionSelect = $(that).siblings(".section");
+            sectionSelect.empty();
+            sectionSelect.append(new Option("Select a Section", null, true, true));
             $.each(response, function(index, item) {
                 sectionSelect.append(new Option(item, item));
             });
         }, 'json');
 }
     $( document ).ready(function() {
+      $(".server").prepend(new Option("Select a Server", null, true, true));  
       $(".server").change(onSelectServer);
       $(".section").change(onSelectSection);
       });
