8bafda294adcb32821652411bf645707d1beb944
Tommy Dorsey <tdorsey@gmail.com>
2017-10-24 15:36:56 -0500

    Continue if a media lookup fails

--- plexsync/plexsync/thirdparty.py
+++ plexsync/plexsync/thirdparty.py
@@ -68,8 +68,7 @@ class ThirdParty():
         if response.status_code == requests.codes.ok:
             return response.json()
         else:
-            print(f"Request failed: {response.status_code}\n{response.content}")
-            return None
+            response.raise_for_status()
         
     def _buildPayload(self, media):
         if self.service == ThirdPartyService.Show:
