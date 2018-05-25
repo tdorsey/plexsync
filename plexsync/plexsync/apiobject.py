b1b3f1a4150b14bedc419e9a23e7cf8acae8d1c2
Tommy Dorsey <tdorsey@users.noreply.github.com>
2018-05-25 22:21:47 +0000

    Make config a singleton. fixes #3

--- plexsync/plexsync/apiobject.py
+++ plexsync/plexsync/apiobject.py
@@ -1,3 +1,4 @@
+import enum
 import logging
 import re
 import requests
@@ -5,9 +6,8 @@ import time
 import traceback
 import urllib
 
-from plexapi.video import Video
 from pprint import pprint
-
+from plexapi.video import Video
 def _hash(self):
     return hash(self.title) 
 
@@ -17,7 +17,7 @@ def _eq(self, other):
 Video.__hash__ = _hash
 Video.__eq__ = _eq
 
-from plexsync.base import *
+from plexsync.base import Base
 from plexsync.thirdparty import ThirdParty, ThirdPartyService
 
 class APIObjectType(enum.Enum):
@@ -26,13 +26,10 @@ class APIObjectType(enum.Enum):
     Episode = "episode"
 
 
-class APIObject(Video):
+class APIObject(Base):
     def __init__(self, video):
       try:
-        b = Base()
-        self.settings = b.getSettings()
-        self.log = logging.getLogger('plexsync')
-
+        super().__init__()
         self.guid = video.guid
         self.type = video.type
         self.overview = video.summary
@@ -41,20 +38,16 @@ class APIObject(Video):
         self.title = video.title
         self.guid = video.guid
         self.image = video.artUrl
-        self.search_term = self._createSearchTerm()
         self.qualityProfileId = None
         self.titleSlug = None
         self.librarySectionID = video.librarySectionID
-        self.log.warn(f"getting download url for {video.title}")
         self.downloadURL = self._getDownloadURL(video)
         self.provider = None
 
         if video.type == "show":
           if self.settings.get(ThirdPartyService.Show.value, "enabled") is True:
              self.provider = ThirdParty(ThirdPartyService.Show)
-          else:
-              self.provider = None
-
+         
           self.seasons = []
           self.title = video.show().title
           self.type = APIObjectType.Show
@@ -62,8 +55,7 @@ class APIObject(Video):
         elif video.type == "episode":
             if self.settings.get(ThirdPartyService.Show.value, "enabled") is True:
               self.provider = ThirdParty(ThirdPartyService.Show)
-            else:
-              self.provider = None
+
             self.guid = video.show().guid
             self.title = video.title
             self.type = APIObjectType.Show
@@ -72,12 +64,14 @@ class APIObject(Video):
            self.type = APIObjectType.Movie
            if self.settings.get(ThirdPartyService.Movie.value, "enabled") is True:
               self.provider = ThirdParty(ThirdPartyService.Movie)
-           else:
-              self.provider = None
         else:
             self.log.debug(f"Unable to determine Object Type for {video}")
             return None
-      except Exception as e:
+
+        if self.provider:
+            self.search_term = self._createSearchTerm()
+
+      except:
         traceback.print_exc()
 
     def _createSearchTerm(self):
@@ -101,8 +95,8 @@ class APIObject(Video):
     def __key(self):
         return (self.guid)
 
-    def __eq__(x, y):
-        return x.__key() == y.__key()
+    def __eq__(self, other):
+        return self.__key() == other.__key()
 
     def __hash__(self):
         return hash(self.__key())
