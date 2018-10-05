d0176c0a258446eb99c3b9f84d7e45c4a4329b69
tdorsey <tdorsey@users.noreply.github.com>
2018-10-04 23:20:07 -0400

    URL encode guids before requesting them

--- plexsync_flask/static/scripts/sockets.js
+++ plexsync_flask/static/scripts/sockets.js
@@ -1,25 +1,44 @@
 var $ = require('jquery')
 var io = require('socket.io-client');
+var urlencode = require("urlencode");
 var socket = io.connect(`${location.protocol}//${document.domain}/plexsync`);
 
+function onComparisonDone(response) {
+        console.log("received comparison_done")
+        let item = {"server" : response.message.server,
+                       "section" : response.message.section  };
+        item.server = urlencode.encode(item.server);
+        item.section = urlencode.encode(item.section);
+        response.message.items.forEach(guid =>
+        {
+           item.guid = urlencode.encode(guid);
+           console.log(item.guid);
+           fetch(`/item/${item.server}/${item.section}/${item.guid}`)
+             .then(function(response) {
+                return response.text();
+                 })
+            .then(function(html) {
+                 $("#comparison_results").append(html);
+                   item.guid = guid ;
+                   console.log(`template rendered for ${item.guid}`);
+                   socket.emit("render_template", item);
+                });
+
+
+     
+         });
+
+}
+
+function onTemplateRendered(response) {
+    console.log("template_rendered")
+        $('#comparison_results').append(response.html);
+}   
 
 
 $(document).ready(function(){
-    socket.on('template_rendered', function(data) {
-        console.log("template_rendered")
-        $('#comparison_results').append(data.html);
-    });
-    socket.on('comparison_done', function(response) {
-        console.log("received comparison_done")
-        let template = {"server" : response.message.server,
-                       "section" : response.message.section  };
-        response.message.items.forEach(guid =>
-		 {
-                      template.guid = guid ;
-                      socket.emit("render_template", template);
-            });
-    });
-   
+    socket.on('template_rendered', onTemplateRendered);
+    socket.on('comparison_done', onComparisonDone);
 });
 
 
